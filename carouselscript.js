5b39962afad07d22f54ad09b3b798e6a71367992
tanav-max61 <tanav.handoo@outlook.com>
2025-12-23 12:22:42 +0530

    Add files via upload

---
+++ carouselscript.js
@@ -0,0 +1,26 @@
+var index = 0;
+function carousel() {
+    var j;
+    var x = document.getElementsByClassName("myslides");
+    for (i = 0; i < x.length; i++) {
+        ``
+
+        x[i].style.display = "none";
+
+    }
+
+    index++;
+
+    if (index > x.length) {
+
+        index = 1;
+
+    }
+
+    x[index - 1].style.display = "block";
+
+    setTimeout(carousel, 2000);
+
+}
+
+carousel(); // Start the carousel when the page loads
